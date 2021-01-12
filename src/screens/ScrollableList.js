af2f165075c94dddaa951df6e1712918447c22e7
samblissard <samblissard@gmail.com>
2021-01-11 20:10:06 -0500

    Demonstrating TextInput

--- src/screens/ScrollableList.js
+++ src/screens/ScrollableList.js
@@ -1,55 +1,79 @@
-import React from 'react';
-import {View, Text, ScrollView, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  Button,
+} from 'react-native';
+
+const initialContactList = [
+  'Darlene Coutee',
+  'Cleopatra Montoro',
+  'Columbus Miracle',
+  'Erin Carrillo',
+  'Elias Shaughnessy',
+  'Wilhemina Vanhook',
+  'Lucretia Badillo',
+  'Ramiro Mccullum',
+  'Orval Spivey',
+  'Mahalia Schear',
+  'Randa Gokey',
+  'Bobby Lile',
+  'Monet Jarboe',
+  'Oleta Herdman',
+  'Myesha Neloms',
+  'Dixie Amidon',
+  'Jackeline Bettes',
+  'Mila Landers',
+  'Seema Flemons',
+  'Alysia Stclair',
+  'Daina Kell',
+  'Pamela Arias',
+  'Lady Upright',
+  'Rachael Lariviere',
+  'Rico Renninger',
+  'Launa Lundell',
+  'Dan Mcconn',
+  'Elsy Chubb',
+  'Maximina Kuykendall',
+  'Chang Kranz',
+];
 
 export default function ScrollableList() {
-  const contacts = [
-    'Darlene Coutee',
-    'Cleopatra Montoro',
-    'Columbus Miracle',
-    'Erin Carrillo',
-    'Elias Shaughnessy',
-    'Wilhemina Vanhook',
-    'Lucretia Badillo',
-    'Ramiro Mccullum',
-    'Orval Spivey',
-    'Mahalia Schear',
-    'Randa Gokey',
-    'Bobby Lile',
-    'Monet Jarboe',
-    'Oleta Herdman',
-    'Myesha Neloms',
-    'Dixie Amidon',
-    'Jackeline Bettes',
-    'Mila Landers',
-    'Seema Flemons',
-    'Alysia Stclair',
-    'Daina Kell',
-    'Pamela Arias',
-    'Lady Upright',
-    'Rachael Lariviere',
-    'Rico Renninger',
-    'Launa Lundell',
-    'Dan Mcconn',
-    'Elsy Chubb',
-    'Maximina Kuykendall',
-    'Chang Kranz',
-  ];
+  const [contacts, setContacts] = useState(initialContactList);
+  const [newContact, setNewContact] = useState('');
+
+  const addContact = () => {
+    setContacts([...contacts, newContact]);
+    setNewContact('');
+  };
 
   return (
-    <ScrollView style={styles.container}>
-      {contacts.map((contact) => (
-        <View>
-          <Text style={styles.text}>{contact}</Text>
-          <View style={styles.divider} />
-        </View>
-      ))}
-    </ScrollView>
+    <View style={styles.container}>
+      <TextInput
+        style={{backgroundColor: 'white', height: 50}}
+        value={newContact}
+        onChangeText={setNewContact}
+      />
+      <Button title="Add Contact" onPress={() => addContact()} />
+      <ScrollView>
+        {contacts.map((contact) => (
+          <View key={contact}>
+            <Text style={styles.text}>{contact}</Text>
+            <View style={styles.divider} />
+          </View>
+        ))}
+      </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     margin: 30,
+    flex: 1,
   },
   text: {
     fontSize: 30,
